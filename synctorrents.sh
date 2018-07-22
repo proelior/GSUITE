1f9aa981d56ef0d3ead725f0174e94c3b5e661dc
proelior <te.proelior@gmail.com>
2018-07-22 08:56:31 +0100

    removed logins

--- synctorrents.sh
+++ synctorrents.sh
@@ -2,5 +2,5 @@
 
 #SSH to fileserver and run synctorrents script to transfer file from dedibox to free up space
 echo 	"Starting synctorrent script [$(date)] ....."
-sshpass -p "emunation" ssh -o StrictHostKeyChecking=no kurt@86.16.231.169 /home/kurt/Sync-Master/synctorrents.sh
+sshpass -p "PASSWORD" ssh -o StrictHostKeyChecking=no USERNAME@86.16.231.169 /home/$USER/Sync-Master/synctorrents.sh
 echo 	"Finished synctorrent script [$(date)] ....."
